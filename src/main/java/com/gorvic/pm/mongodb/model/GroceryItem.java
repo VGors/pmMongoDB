ea6dfcba5c62997aba06e3aacfd8f07125fa3fc5
Victor <v.gorshenev@gmail.com>
2023-02-25 10:14:24 +0500

    Errors corrected

--- src/main/java/com/gorvic/pm/mongodb/model/GroceryItem.java
+++ src/main/java/com/gorvic/pm/mongodb/model/GroceryItem.java
@@ -18,4 +18,36 @@ public class GroceryItem {
         this.quantity = quantity;
         this.category = category;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
